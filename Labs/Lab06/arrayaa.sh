51a3966dc3da1588308bce00d46dc278a92c2db5
gcolonro-bash <gcolonro@charlotte.edu>
2025-09-25 16:53:51 -0400

    Add files via upload

--- Labs/Lab06/arrayaa.sh
+++ Labs/Lab06/arrayaa.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+amino_acids=(
+    "Methionine"
+    "Leucine"
+    "Cysteine"
+    "Alanine"
+    "Valine"
+    "Tyrosine"
+    "Proline"
+)
+
+for amino_acid in "${amino_acids[@]}"; do
+    length=${#amino_acid}
+    echo "$amino_acid (length: $length)"
+done
